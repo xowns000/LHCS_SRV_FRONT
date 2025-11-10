969cfdac675efc19f7d6b431aad42491069fdcd2
jy_na <njy305@hkcloud.co.kr>
2025-11-10 17:52:49 +0900

    제외조건 이력 조회 화면 개발중

---
+++ src/views/page/SVY_M0004.vue
@@ -0,0 +1,560 @@
+<template>
+  <div class="pl-container">
+    <div class="pl-card-wrap">
+      <!-- sub top -->
+      <compo-sub-layout-top  >
+        <template slot="search">
+          <div class="pl-form-inline-wrap">
+            <div class="pl-form-inline">
+              <span class="pl-label">
+                센터구분
+              </span>
+              <div class="pl-desc">
+                <v-select
+                  class="pl-form"
+                  :items="SRCH_DEPT_LIST"
+                  placeholder="선택하세요"
+                  v-model="SRCH_DEPT_ID"
+                ></v-select>
+              </div>
+            </div>
+            <div class="pl-form-inline">
+              <span class="pl-label">
+                진행 년도
+              </span>
+              <div class="pl-desc">
+                <v-select
+                    class="pl-form"
+                    :items="srvyYrItems"
+                    v-model="SRCH_SRVY_YR"
+                    placeholder="선택하세요"
+                ></v-select>
+              </div>
+            </div>
+            <div class="pl-form-inline">
+              <span class="pl-label">계획 구분</span>
+              <div class="pl-desc">
+                <v-select
+                    class="pl-form"
+                    :items="mixin_common_code_get(this.common_code, 'SUVY_CL', '전체')"
+                    placeholder="선택하세요"
+                    v-model="SRCH_SRVY_SE_CD"
+                    @change="getSrvyNm"
+                ></v-select>
+              </div>
+            </div>
+            <div class="pl-form-inline">
+              <span class="pl-label">설문조사 명</span>
+              <div class="pl-desc">
+                <v-select class="pl-form is-mid"
+                          :items="srvyNmItems"
+                          v-model="SRCH_SRVY_NM"
+                          return-object
+                          placeholder="선택하세요"
+                >
+                </v-select>
+              </div>
+            </div>
+            <div class="pl-form-inline">
+              <span class="pl-label">
+                제외조건 구분
+              </span>
+              <div class="pl-desc">
+                <v-select
+                  class="pl-form"
+                  :items="this.mixin_common_code_get(this.common_code, 'EXL_COND_TY', '전체')"
+                  placeholder="선택하세요"
+                  v-model="SRCH_EXL_COND_SE_CD"
+                ></v-select>
+              </div>
+            </div>
+            <div class="pl-form-inline">
+              <span class="pl-label">
+                제외조건
+              </span>
+              <div class="pl-desc">
+                <v-select
+                  class="pl-form"
+                  :items="this.mixin_common_code_get(this.common_code, 'EXL_COND', '전체')"
+                  placeholder="선택하세요"
+                  v-model="SRCH_EXL_COND_CD"
+                ></v-select>
+              </div>
+            </div>
+            <div class="pl-form-inline">
+              <span class="pl-label">
+                제외조건 값
+              </span>
+              <div class="pl-desc">
+                <v-text-field
+                  class="pl-form is-lg"
+                  placeholder="검색어 입력"
+                  v-model="SRCH_EXL_COND_CN"
+                  @keydown.enter="getGridList(false)"
+                />
+              </div>
+            </div>
+            <v-btn class="pl-btn is-icon" @click="getGridList(false)">
+              <span class="pl-icon20 search"></span>
+              조회
+            </v-btn>
+
+          </div>
+        </template>
+      </compo-sub-layout-top>
+      <!-- sub content -->
+      <div class="pl-card-body">
+        <div class="pl-grid-top">
+          <div class="pl-grid-top-left">
+            <v-btn class="pl-btn is-icon is-sub" @click="mixin_showDialog('RegExlCond')" :disabled="selectedRow.ROW_NUMBER != undefined ? false : true">
+              <span class="pl-icon20 document"></span>
+              선택항목 상세
+            </v-btn>
+          </div>
+          <div class="pl-grid-top-utils">
+            <span class="pl-counter">전체 <em class="pl-1">({{ gridTotalCnt }})</em> 건</span>
+          </div>
+        </div>
+        <v-data-table
+          class="pl-grid has-control"
+          style="cursor:pointer;"
+          show-select
+          :headers="gridDataHeaders"
+          :items="gridItems"
+          fixed-header
+          item-key="ROW_NUMBER"
+          height="562px"
+          :items-per-page="ROW_PER_PAGE"
+          :item-class="isActiveRow"
+          hide-default-footer
+          :page.sync="page"
+          @page-count="pageCount = $event"
+          @click:row="rowSelect"
+          @dblclick:row="mixin_showDialog('RegExlCond')"
+          v-model="selectedData"
+          no-data-text="등록된 데이터가 없습니다."
+        >
+        </v-data-table>
+        <div class="pl-pager">
+          <div class="pl-pager-row">
+            <span>페이지당 항목 수</span>
+            <v-select
+              class="pl-form"
+              :value="ROW_PER_PAGE"
+              :items="perPage"
+              :item-text="toString(ROW_PER_PAGE)"
+              @change="ROW_PER_PAGE = parseInt($event, 10);"
+            ></v-select>
+          </div>
+          <v-pagination
+            v-model="page"
+            :length="pageCount"
+            circle
+            :total-visible="7">
+          </v-pagination>
+
+          <span class="pl-pager-period">
+            보기 {{ mixin_getPagePeriod(gridItems, page) }} / {{ gridItems.length }}
+            <compo-tooltip-btn
+              TitleProp="다음 검색"
+              ClassProp="pl-tooltip-btn is-line"
+              IconProp="pl-icon20 arrow-next-paging"
+              TooltipPositionProp="bottom"
+              :DisabledProp = "nextDisabled"
+              @btnClick="getGridList(true)"
+            ></compo-tooltip-btn>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <!-- dialog -->
+    <v-dialog
+      v-model="dialogRegExlCond"
+      content-class="dialog-draggable"
+      hide-overlay
+      :retain-focus="false">
+      <div class="draggable-area">drag area</div>
+      <compo-dialog
+        :headerTitle="SRVY_EXL_COND_ID=='' ? '제외조건 등록' : '제외조건 수정'"
+        @hide="mixin_hideDialog('RegExlCond')">
+        <template slot="body">
+          <v-form ref="form">
+            <div class="pl-form-inline-wrap vertical mt-2">
+              <div class="pl-form-inline">
+                <span class="pl-label">
+                  센터 구분
+                  <v-icon class="pl-icon20 required"></v-icon>
+                </span>
+                <div class="pl-desc">
+                  <v-select
+                    class="pl-form"
+                    :items="DEPT_LIST"
+                    placeholder="선택하세요"
+                    v-model="DEPT_ID"
+                    :rules="detailValidateRules.DEPT_ID"
+                  ></v-select>
+                </div>
+              </div>
+              <div class="pl-form-inline">
+                <span class="pl-label">
+                  제외조건 구분
+                  <v-icon class="pl-icon20 required"></v-icon>
+                </span>
+                <div class="pl-desc">
+                  <v-select
+                    class="pl-form"
+                    :items="this.mixin_common_code_get(this.common_code, 'EXL_COND_TY')"
+                    placeholder="선택하세요"
+                    v-model="EXL_COND_SE_CD"
+                    :rules="detailValidateRules.EXL_COND_SE_CD"
+                  ></v-select>
+                </div>
+              </div>
+              <div class="pl-form-inline">
+                <span class="pl-label">
+                  제외조건
+                  <v-icon class="pl-icon20 required"></v-icon>
+                </span>
+                <div class="pl-desc">
+                  <v-select
+                    class="pl-form"
+                    :items="this.mixin_common_code_get(this.common_code, 'EXL_COND')"
+                    placeholder="선택하세요"
+                    v-model="EXL_COND_CD"
+                    :rules="detailValidateRules.EXL_COND_CD"
+                  ></v-select>
+                </div>
+              </div>
+              <div class="pl-form-inline">
+                <span class="pl-label">
+                  제외조건 값
+                  <v-icon class="pl-icon20 required"></v-icon>
+                </span>
+                  <div class="pl-desc">
+                    <v-text-field
+                    class="pl-form is-lg"
+                    placeholder="검색어 입력"
+                    v-model="EXL_COND_CN"
+                    :rules="detailValidateRules.EXL_COND_CN"
+                  />
+                </div>
+              </div>
+              <div class="pl-form-inline">
+                <span class="pl-label">
+                  사용여부
+                  <v-icon class="pl-icon20 required"></v-icon>
+                </span>
+                <div class="pl-desc">
+                  <v-select
+                    class="pl-form"
+                    :items="this.mixin_common_code_get(this.common_code, 'USE_WT')"
+                    placeholder="선택하세요"
+                    v-model="USE_YN"
+                    :rules="detailValidateRules.USE_YN"
+                  ></v-select>
+                </div>
+              </div>
+            </div>
+          </v-form>
+        </template>
+        <template slot="footer">
+          <v-btn class="pl-btn is-sub" @click="mixin_hideDialog('RegExlCond')">닫기</v-btn>
+          <v-btn class="pl-btn" @click="saveBtn()">저장</v-btn>
+        </template>
+      </compo-dialog>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"MENU_SVY_M0002", //name은 'MENU_' + 파일명 조합
+
+ components: {
+   
+  },
+ data() {
+    return {
+      common_code: [],
+      srvyYrItems: [],
+
+      detailValidateRules: {
+        DEPT_ID: [
+          v => !!v || '센터구분을 선택하세요',
+        ],
+        EXL_COND_SE_CD: [
+          v => !!v || '제외조건 구분을 선택하세요.',
+        ],
+        EXL_COND_CD: [
+          v => !!v || '제외조건을 선택하세요.',
+        ],
+        EXL_COND_CN: [
+          v => !!v || '제외조건 값을 입력하세요.',
+        ],
+        USE_YN: [
+          v => !!v || '사용여부를 선택하세요.',
+        ],
+      },
+
+      SRCH_DEPT_LIST:[],
+      SRCH_DEPT_ID:'',
+      SRCH_EXL_COND_SE_CD:'',
+      SRCH_EXL_COND_CD:'', 
+      SRCH_EXL_COND_CN:'',
+      SRCH_USE_YN:'Y',
+      SRCH_DEL_YN:'N',
+      SRCH_SRVY_YR: '',
+      SRCH_SRVY_SE_CD: '',
+      SRCH_SRVY_NM : {},
+      srvyNmItems : [],
+
+
+      DEPT_LIST:[],
+      DEPT_ID:'',
+      EXL_COND_SE_CD:'',
+      EXL_COND_CD:'', 
+      EXL_COND_CN:'',
+      USE_YN:'Y',
+
+      selectedRow: {},
+      SRVY_EXL_COND_ID:'',
+
+      // grid
+      page: 1,
+      pageCount: 0,
+      perPage: [15,30,50,100],
+      ROW_PER_PAGE: 15,
+      selectedData: [],      // 그리드 체크박스 값
+      pagination:{
+        page:1,
+        rowsPerPage: 500
+      },
+      nextDisabled: false,
+      gridItems: [],
+      gridTotalCnt: 0,
+
+      gridDataHeaders: [
+          { text: '번호', value: 'ROW_NUMBER', align: 'center', width: '80px' },
+          { text: '부서', value: 'DEPT_NM', align: 'left', width: '120px'},
+          { text: '조건 구분', value: 'EXL_COND_SE_NM', align: 'center', width: '200px'},
+          { text: '조건', value: 'EXL_COND_NM', align: 'left', width: '200px' },
+          { text: '조건 값', value: 'EXL_COND_CN', align: 'left', width: '300px'},
+          { text: '등록자', value: 'RGTR_NM', align: 'left', width: '120px'},
+          { text: '등록일', value: 'REG_DT_F', align: 'left', width: '120px'},
+          { text: '수정자', value: 'MDFR_NM', align: 'left', width: '120px'},
+          { text: '수정일', value: 'MDFCN_DT_F', align: 'left', width: '120px'},
+      ],
+
+      dialogRegExlCond:false,
+    }
+  },
+  watch: {
+
+    SRCH_SRVY_SE_CD(){
+      if(!this.SRCH_SRVY_SE_CD){
+        this.SRCH_SRVY_NM = {}
+      }
+    }
+  },
+
+  computed: {
+    
+  },
+
+  async created() {
+    // 공통코드설정
+    let codeName = ['EXL_COND_TY', 'EXL_COND', 'USE_WT','SUVY_CL'];
+    this.common_code = await this.mixin_common_code_get_all(codeName);
+
+    this.getDeptList();
+    this.getGridList();
+  },
+
+  mounted() {
+    //진행년도
+    const date = new Date();
+    const curYear = date.getFullYear();
+    this.srvyYrItems.push({ text: '전체', value: '' });   // default
+    for (let i = 0; i < 10; i++) {
+      this.srvyYrItems.push({ text: `${curYear - i}년`, value: (curYear - i).toString() });
+    }
+    this.SRCH_SRVY_YR =  curYear.toString();
+  },
+  
+  methods: {
+    async getDeptList(){
+      this.SRCH_DEPT_LIST=[{
+        text:'전체'
+        , value:''
+      }];
+      this.DEPT_LIST=[];
+      let deptCdList = await this.mixin_getDeptList();
+      for(let i=0;i<deptCdList.length;i++){
+        let list = {
+          text:deptCdList[i].DEPT_NM
+          , value: deptCdList[i].DEPT_ID
+        }
+        this.SRCH_DEPT_LIST.push(list);
+        this.DEPT_LIST.push(list);
+      }
+    },
+
+    rowSelect(item){
+      this.selectedRow = item;
+      this.SRVY_EXL_COND_ID = item.SRVY_EXL_COND_ID;
+      
+      this.DEPT_ID=item.DEPT_ID;
+      this.EXL_COND_SE_CD=item.EXL_COND_SE_CD;
+      this.EXL_COND_CD=item.EXL_COND_CD;
+      this.EXL_COND_CN=item.EXL_COND_CN;
+      this.USE_YN=item.USE_YN;
+    },
+    
+    async getGridList(next){
+      //다음버튼 클릭 유무
+      if (!next){
+        this.pagination.page = 1; //페이징 처리 초기화
+        this.selectedData = [];
+        this.gridTotalCnt = 0;
+      }
+
+      let sUrl = '/api/svy/exclusion/selectConditionList';
+      let postParam = {
+        DEPT_ID: this.SRCH_DEPT_ID
+        , EXL_COND_SE_CD: this.SRCH_EXL_COND_SE_CD
+        , EXL_COND_CD : this.SRCH_EXL_COND_CD
+        , EXL_COND_CN : this.SRCH_EXL_COND_CN
+        , USE_YN : this.SRCH_USE_YN
+        , DEL_YN : this.SRCH_DEL_YN
+      }
+
+      let headParam = {
+        head : {
+          PAGING: 'Y',
+          ROW_CNT: this.pagination.rowsPerPage,
+          PAGES_CNT: this.pagination.page
+        }
+      }
+
+      let response = await this.common_postCall(sUrl, postParam, headParam);
+
+      if(!response.HEADER.ERROR_FLAG) {
+        if(next){
+          // 다음검색
+          let tempDataText = response.DATA;
+          let idx = this.gridItems.length + 1;
+          for(let i in tempDataText){
+            tempDataText[i]["ROW_NUMBER"]= idx++;
+          }
+          this.gridItems = [...this.gridItems, ...tempDataText];
+          
+        }else{
+          // 조회
+          this.gridItems = response.DATA;
+        }
+        // 전체 건수
+        if(this.gridItems.length > 0) this.gridTotalCnt = response.DATA[0].TWB_PAGING_TOT_COUNT;
+
+        // 다음검색 버튼 활성화 여부
+        if(response.HEADER.next !== null && response.HEADER.next !== undefined){
+          if(response.HEADER.next === true){
+            this.nextDisabled = false // 버튼 활성화
+          }else{
+            this.nextDisabled = true  // 버튼 비활성화
+          }
+        }
+
+        this.pagination.page += 1;
+        // this.page=1;
+      }
+    },
+
+    regBtn(){
+      this.initSel();
+      this.mixin_showDialog('RegExlCond');
+    },
+
+    deleteBtn(){
+
+    },
+
+    initSel(){
+      this.selectedRow = {};
+      this.SRVY_EXL_COND_ID = '';
+
+      this.DEPT_ID='';
+      this.EXL_COND_SE_CD='';
+      this.EXL_COND_CD='';
+      this.EXL_COND_CN='';
+      this.USE_YN='Y';
+    },
+    
+    //row 선택 활성화
+    isActiveRow(item) {
+      const activeClass = item === this.selectedRow ? "active" : "";
+      return activeClass;
+    },
+
+    //저장버튼 클릭
+    async saveBtn(){
+      let sUrl = '/api/svy/exclusion/mergeCondition';
+      let postParam = {
+        SRVY_EXL_COND_ID: this.SRVY_EXL_COND_ID
+        , DEPT_ID: this.DEPT_ID
+        , EXL_COND_SE_CD: this.EXL_COND_SE_CD
+        , EXL_COND_CD : this.EXL_COND_CD
+        , EXL_COND_CN : this.EXL_COND_CN
+        , USE_YN : this.USE_YN
+      }
+
+      let headParam = {
+        head : {
+        }
+      }
+
+      let response = await this.common_postCall(sUrl, postParam, headParam);
+
+      if(!response.HEADER.ERROR_FLAG) {
+        this.mixin_hideDialog('RegExlCond');
+        this.getGridList();
+        this.showToast({  msg: '처리가 완료되었습니다.', class: 'success', hasToastIcon: true, icon: 'mdi-checkbox-marked-circle' , time: 2000 })
+      }
+    },
+    async getSrvyNm(){
+      const sUrl = '/api/svy/makeitems/selectcombomakeitems';
+      const postParam = {
+        SRVY_YR : this.SRVY_YR             //진행년도
+        ,STTS_CD : this.STTS_CD         //진행상태
+        ,SRVY_SE_CD : this.SRVY_SE_CD        //계획구분
+      }
+      const headParam = {
+        head: {
+          'SERVICE' : 'svy.plan.selectcombomakeitems',
+          'METHOD' : 'selectcombomakeitems',
+          'TYPE' : 'BIZ_SERVICE',
+        }
+      }
+
+      let resData = await this.common_postCall(sUrl, postParam, headParam );
+
+
+      if (resData.HEADER.ERROR_FLAG)
+      {
+        this.setErrMsg(resData);
+      }
+      else
+      {
+
+        this.SRCH_SRVY_NM = {};
+        this.srvyNmItems = resData.DATA.map(item => ({ text: this.mixin_decode(item.TEXT), value: item.VALUE }));
+        if (this.srvyNmItems.length > 0) this.SRCH_SRVY_NM = this.mixin_decode(resData.DATA[0].VALUE);
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
