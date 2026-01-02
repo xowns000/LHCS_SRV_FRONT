2fc814003387c9d4c034f6dee85817717115ee95
ktj <ktj@hkcloud.co.kr>
2026-01-02 17:28:25 +0900

    제외조건 변경이력 있을때만 표시로 변경

--- src/views/page/SVY_M0002.vue
+++ src/views/page/SVY_M0002.vue
@@ -412,6 +412,7 @@
               </div>
             </div>
             <div 
+              v-if="EXL_COND_HSTRY_ITEMS.length>0"  
               class="is-border mt-2"
             >
               <h2 class="pl-subtit pb-2">제외조건 변경이력</h2>
@@ -422,12 +423,12 @@
                   :items="EXL_COND_HSTRY_ITEMS"
                   fixed-header
                   item-key="ROW_NUMBER"
-                  height="115px"
+                  height="200px"
                   :items-per-page="EXL_COND_HSTRY_ITEMS.length"
                   :item-class="isActiveHstryRow"
                   hide-default-footer
                   @click:row="hstryRowSelect"
-                  no-data-text="변경이력이 없습니다"
+                  no-data-text="등록된 데이터가 없습니다."
                 >
                   <template v-slot:item.EXL_COND_CN="{ item }">
                     <v-tooltip 
